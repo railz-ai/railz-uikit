c87940abfaf6ddbf1fe167a9720f3d6e1102b6ed
abisolaadeniran <abisolaadeniran@users.noreply.github.com>
2022-06-16 22:49:20 -0300

    chore: Cleanup tests

--- design-icons/packages/railz-icons/src/components/my-component/my-component.spec.ts
+++ design-icons/packages/railz-icons/src/components/my-component/my-component.spec.ts
@@ -1,9 +1,9 @@
-import {newSpecPage} from '@stencil/core/testing';
-import {MyComponent} from './my-component';
+import { newSpecPage } from '@stencil/core/testing';
+import { MyComponent } from './my-component';
 
 describe('my-component', () => {
   it('renders', async () => {
-    const {root} = await newSpecPage({
+    const { root } = await newSpecPage({
       components: [MyComponent],
       html: '<my-component></my-component>',
     });
