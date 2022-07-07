815b6c922d8e73dae347ba45946400df7787d460
christopher <christopher@christophers-MacBook-Pro.local>
2022-07-07 16:36:43 -0400

    fix: format provider name

--- design-components/packages/railz-components/src/utils/utils.ts
+++ design-components/packages/railz-components/src/utils/utils.ts
@@ -1,3 +1,9 @@
 export function format(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ' ' + middle : '') + (last ? ' ' + last : '');
 }
+
+export const formatServiceName = (name: string): string => {
+  return name.replace(/([A-Z])/g, ' $1').replace(/^./, str => {
+    return str.toUpperCase();
+  });
+};
