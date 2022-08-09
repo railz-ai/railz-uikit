40a438d6c37a70b9fb01643e70aeb422217ead17
Zach Forrester <zach@Zachs-Work-MacBook-Pro.local>
2022-08-09 11:13:02 -0400

    created shaded background for themed button

--- design-components/packages/railz-components/src/components/railz-button/railz-button.tsx
+++ design-components/packages/railz-components/src/components/railz-button/railz-button.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-no-bind */
 import { Component, Host, h, Prop, Event, EventEmitter } from '@stencil/core';
 
-import * as iconList from '../../assets/icons/index.json';
+// import * as iconList from '../../assets/icons/index.json';
 
 /**
  * @description This is the button
@@ -35,23 +35,23 @@ export class RailzButton {
     return `${this.type} ${this.size} ${this.shape} ${this.grow ? 'grow' : ''} ${this.loading ? 'loading' : ''} ${this.isDisabled ? 'disabled' : ''}`;
   }
 
-  private renderIcon(): string {
-    if (this.icon && iconList.icons.some(icon => icon.name === this.icon)) {
-      return <railz-icon icon={this.icon} />;
-    }
-  }
+  // private renderIcon(): string {
+  //   if (this.icon && iconList.icons.some(icon => icon.name === this.icon)) {
+  //     return <railz-icon icon={this.icon} />;
+  //   }
+  // }
 
   render(): HTMLElement {
     return (
       <Host class={`button ${this.buttonStyles()}`}>
         {this.href ? (
           <a href={this.href} target={this.target} class={`button ${this.buttonStyles()}`}>
-            {this.renderIcon()}
+            {/* {this.renderIcon()} */}
             <span class="label">{this.label}</span>
           </a>
         ) : (
           <button onClick={event => this.handleClick(event)} class={`button ${this.buttonStyles()}`} disabled={this.isDisabled}>
-            {this.renderIcon()}
+            {/* {this.renderIcon()} */}
             {this.loading ? <span class="loading-indicator"></span> : null}
             <span class="label">{this.loading ? 'Loading...' : this.label}</span>
           </button>
