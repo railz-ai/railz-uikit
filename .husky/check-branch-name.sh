fd0e2bb90b70cd350b34912b1e236ccfcb6a4fcc
Santiago Persico <spersico@railz.ai>
2023-10-23 19:13:07 -0300

    improve husky branch name checker error msg

--- .husky/check-branch-name.sh
+++ .husky/check-branch-name.sh
@@ -10,6 +10,9 @@ fi
 valid_regex='^((bugfix|feature|hotfix|chore)\/[a-zA-Z0-9\-]+)$'
 
 message="Your branch name does not conform to: $valid_regex."
+message+="\n\n"
+message+="Examples: bugfix/fix-bug, feature/add-feature, hotfix/fix-bug, chore/update-readme"
+message+="\n\n"
 
 if [[ ! $branch_name =~ $valid_regex ]]; then
     echo "$message"
